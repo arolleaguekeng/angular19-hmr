f3c1eaa3d1686a1cf244ec5303624a05505ae9ca
arolleaguekeng <arolleaguekeng@gmail.com>
2025-05-12 22:43:46 +0100

    basic exemple of hrm

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'HMR Demo';
+  counter = 0;
+  name = '';
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+}
